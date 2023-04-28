9250429f1c3f083572124b449efce785f977966e
wuxjian <wuxj@wbpharma.com>
2023-04-28 15:18:31 +0800

    Revert "引入antd"
    This reverts commit 9c3241f1116f6ac90d08ac768ba231c73ea3110f.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,9 @@
 import './App.css';
-import {DatePicker} from "antd";
 
 function App() {
     return (
         <div id="App">
-            <DatePicker />
+            wails
         </div>
     )
 }
